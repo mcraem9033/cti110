8fb6fabfd0f20e11f871afc70e8b0113d8163475
mcraem9033 <mcraem9033@student.faytechcc.edu>
2025-04-02 11:00:23 -0400

    Add files via upload
    “First submission”.

---
+++ P1LAB1_McRaeMarcus..py
@@ -0,0 +1,12 @@
+# Marcus McRae
+# 04/01/25
+# P1LAB1
+# Intro to CTL-110
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+welcome ="Welcome"
+
+print("Hello,", first_name, last_name, "Welcome to CTI-110")
